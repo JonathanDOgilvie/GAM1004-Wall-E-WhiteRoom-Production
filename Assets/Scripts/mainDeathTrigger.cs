697c8d0d6c00024c68c28bc63d82caf74a5a30bd
Jonathan Ogilvie <jo705@live.mdx.ac.uk>
2020-03-26 15:08:45 +0000

    MainDeathTrigger

---
+++ Assets/Scripts/mainDeathTrigger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mainDeathTrigger : MonoBehaviour
+{
+
+    public Transform spawn = null;
+    public CharacterController player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        player.enabled = false;
+        other.transform.position = spawn.position;
+        player.enabled = true;
+    }
+}//End of Script
